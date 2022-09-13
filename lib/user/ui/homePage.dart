3da7ce60666af0a33f3a4c54a43b9465051ab7c6
Andrés F. Rodriguez <82066965+Slorq-code@users.noreply.github.com>
2022-09-13 13:19:21 -0500

    cambio foto HomePage

--- lib/user/ui/homePage.dart
+++ lib/user/ui/homePage.dart
@@ -73,7 +73,8 @@ class _HomePageState extends State<HomePage> {
                                 fit: BoxFit.fill))),
                   ),
                   SizedBox(
-                    height: Sizes.height * 0.28,
+                    height: Sizes.height * 0.28
+                    ,
                   ),
                   GestureDetector(
                     onTap: () {
@@ -84,7 +85,8 @@ class _HomePageState extends State<HomePage> {
                       height: Sizes.height * 0.3,
                       decoration: const BoxDecoration(
                           image: DecorationImage(
-                              image: AssetImage("assets/images/image_04.png"),
+                              image:
+                                  AssetImage("assets/images/image_04_01.png"),
                               fit: BoxFit.cover)),
                     ),
                   ),
